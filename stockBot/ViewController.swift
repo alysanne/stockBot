da5b7196fb551177d082495a500ef5425012c333
Natalia Fernández <natalia.fpintos@gmail.com>
2019-09-28 18:41:37 +0100

    Initial Commit

---
+++ stockBot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stockBot
+//
+//  Created by Fernández Pintos, Natalia (Associate Software Developer) on 28/09/2019.
+//  Copyright © 2019 Natalia Fernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
